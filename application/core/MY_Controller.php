68b32c6ea272fd5ca124f99ab3a944f217bffce1
lannguyen1980 <buubecas@yahoo.com>
2018-12-22 12:50:34 +0700

    Update MY_Controller.php

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -5,7 +5,7 @@ class MY_Controller extends CI_Controller
 {
 	public function __construct()
 	{
-		parent::__construct();
+		// parent::__construct();
 
         /* COMMON :: ADMIN & PUBLIC */
         /* Load */
